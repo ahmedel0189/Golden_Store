0d2c158f4033548be621c3336b0b1f417e96b331
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-09-30 20:23:16 +0300

    doing some modifing

--- lib/layers/ui/screens/homae_page.dart
+++ lib/layers/ui/screens/homae_page.dart
@@ -5,6 +5,10 @@ class HomaePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body:Center(child: Text(
+        'Hi Ahmed',style: TextStyle(fontWeight: FontWeight.bold),
+      ),)
+    );
   }
 }
