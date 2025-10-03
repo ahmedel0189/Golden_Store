1b5482778b812c88303d34a6bded9074e0f7b3ef
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-10-03 17:17:24 +0300

    making  cuibt to the liked productes

--- lib/layers/domain/logic/cubit/products_cubit.dart
+++ lib/layers/domain/logic/cubit/products_cubit.dart
@@ -1,7 +1,29 @@
 import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
 
 part 'products_state.dart';
 
 class ProductsCubit extends Cubit<ProductsState> {
-  ProductsCubit() : super(ProductsInitial());
+  ProductsCubit() : super(const ProductsInitial());
+
+  // Map to store liked products (by id)
+  final Map<int, bool> _likedProducts = {};
+
+  // Getter to expose liked products (immutable copy)
+  Map<int, bool> get likedProducts => Map.unmodifiable(_likedProducts);
+
+  // Toggle like/unlike for a product
+  void toggleLike(int productId) {
+    final current = _likedProducts[productId] ?? false;
+    _likedProducts[productId] = !current;
+
+    // Emit updated state
+    emit(LikedProducts(Map.from(_likedProducts)));
+  }
+
+  // Reset all liked products
+  void resetLikes() {
+    _likedProducts.clear();
+    emit(LikedProducts(Map.from(_likedProducts)));
+  }
 }
--- lib/layers/domain/logic/cubit/products_state.dart
+++ lib/layers/domain/logic/cubit/products_state.dart
@@ -1,5 +1,21 @@
 part of 'products_cubit.dart';
 
-sealed class ProductsState {}
+sealed class ProductsState extends Equatable {
+  const ProductsState(); 
+}
 
-final class ProductsInitial extends ProductsState {}
+final class ProductsInitial extends ProductsState {
+  const ProductsInitial();
+
+  @override
+  List<Object?> get props => [];
+}
+
+final class LikedProducts extends ProductsState {
+  final Map<int, bool> likedProducts;
+
+  const LikedProducts(this.likedProducts);
+
+  @override
+  List<Object?> get props => [likedProducts];
+}
